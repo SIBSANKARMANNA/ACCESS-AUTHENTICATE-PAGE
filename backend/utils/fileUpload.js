aebabfc890f951a89fcb447f3676885677f24489
SIBSANKAR MANNA <108569179+SIBSANKARMANNA@users.noreply.github.com>
2025-01-06 23:32:24 +0530

    Add files via upload

---
+++ backend/utils/fileUpload.js
@@ -0,0 +1,28 @@
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}-${file.originalname}`);
+    },
+});
+
+const upload = multer({
+    storage,
+    fileFilter: (req, file, cb) => {
+        const fileTypes = /jpeg|jpg|png/;
+        const extname = fileTypes.test(path.extname(file.originalname).toLowerCase());
+        const mimeType = fileTypes.test(file.mimetype);
+
+        if (extname && mimeType) {
+            return cb(null, true);
+        } else {
+            cb('Error: Images only!');
+        }
+    },
+});
+
+module.exports = upload;
